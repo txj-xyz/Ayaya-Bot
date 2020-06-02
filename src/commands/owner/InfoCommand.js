971472c2dde406681fa5d461e07cc0969d49a058
JayEffTree <jfinelli3@gmail.com>
2020-06-02 20:09:51 +0200

    info command

---
+++ src/commands/owner/InfoCommand.js
@@ -0,0 +1,27 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const humanizeDuration = require('humanize-duration')
+
+module.exports = class InfoCommand extends BaseCommand {
+  constructor() {
+    super('info', 'owner', [], 'Prints debug information to the channel.');
+  }
+
+  async run(client, message, args) {
+
+    let loading = await message.channel.send(client.resource.loading())
+    loading.edit(client.resource.embed()
+        .setTitle('Aya Stats')
+        .setColor('#03a9fc')
+        .setTimestamp()
+        .setDescription(
+            `\`\`\`\nUptime: ${humanizeDuration(client.uptime)}\n`+
+            `Users: ${client.users.cache.size}\n`+
+            `Guilds: ${client.guilds.cache.size}\n`+
+            `Language: NodeJS\n`+
+            `Memory Usage: ${process.memoryUsage().rss / 1000000}\n`+
+            `WS Latency: ${client.ws.ping}ms\n`+
+            `Owner: TXJ#5664\`\`\``
+        )
+    )
+  }
+}
