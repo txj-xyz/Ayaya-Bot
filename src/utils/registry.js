d040526749b57d337ac4122db07fe68b6f3986ff
JayEffTree <jfinelli3@gmail.com>
2020-06-02 20:10:19 +0200

    why does this not work??

--- src/utils/registry.js
+++ src/utils/registry.js
@@ -10,19 +10,24 @@ async function registerCommands(client, dir = '') {
   for (const file of files) {
     const stat = await fs.lstat(path.join(filePath, file));
     if (stat.isDirectory()) registerCommands(client, path.join(dir, file));
-    if (file.endsWith('.js')) {
-      const Command = require(path.join(filePath, file));
-      if (Command.prototype instanceof BaseCommand) {
-        const cmd = new Command();
-        client.commands.set(cmd.name, cmd);
-        console.log(`[INFO] Loaded - [${cmd.name} - ${cmd.description}]`)
-        cmd.aliases.forEach(async (alias) => {
-          client.commands.set(alias, cmd);
-          console.log(`[INFO] Loaded alias - [${alias}]`)
-        });
-      }
+      // try{
+        if (file.endsWith('.js')) {
+          const Command = require(path.join(filePath, file));
+          if (Command.prototype instanceof BaseCommand) {
+            const cmd = new Command();
+            client.commands.set(cmd.name, cmd);
+            console.log(`[INFO] Loaded - [${cmd.name} - ${cmd.description}]`)
+            cmd.aliases.forEach(async (alias) => {
+              client.commands.set(alias, cmd);
+              console.log(`[INFO] Loaded alias - [${alias}]`)
+            });
+          }
+        }
+      // } catch(e) {
+      //   throw `[ERROR] - Loading Failed -  [${file.substring(0, file.length-3)}]`
+      // }
     }
-  }
+    
 }
 
 async function reloadCommands(client, dir = '../commands') {
@@ -32,20 +37,24 @@ async function reloadCommands(client, dir = '../commands') {
   for (const file of files) {
     const stat = await fs.lstat(path.join(filePath, file));
     if (stat.isDirectory()) reloadCommands(client, path.join(dir, file));
-    if (file.endsWith('.js')) {
-      delete require.cache[require.resolve(path.join(filePath, file))]
-      const Command = require(path.join(filePath, file));
-      if (Command.prototype instanceof BaseCommand) {
-        const cmd = new Command();
-        await client.commands.set(cmd.name, cmd);
-        console.log(`[INFO] Reloaded - [${cmd.name}]`)
-        cmd.aliases.forEach(async (alias) => {
-          delete require.cache[require.resolve(path.join(filePath, file))]
-          await client.commands.set(alias, cmd);
-          console.log(`[INFO] Reloaded alias - [${alias}]`)
-        });
+    // try{
+      if (file.endsWith('.js')) {
+        delete require.cache[require.resolve(path.join(filePath, file))]
+        const Command = require(path.join(filePath, file));
+        if (Command.prototype instanceof BaseCommand) {
+          const cmd = new Command();
+          await client.commands.set(cmd.name, cmd);
+          console.log(`[INFO] Reloaded - [${cmd.name}]`)
+          cmd.aliases.forEach(async (alias) => {
+            delete require.cache[require.resolve(path.join(filePath, file))]
+            await client.commands.set(alias, cmd);
+            console.log(`[INFO] Reloaded alias - [${alias}]`)
+          });
+        }
       }
-    }
+    // }catch(e){
+    //   throw `[ERROR] - Reloading Failed -  [${file.substring(0, file.length-3)}]`;
+    // }
   }
 }
 
