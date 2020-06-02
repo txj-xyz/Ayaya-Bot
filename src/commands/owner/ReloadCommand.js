fe111b6aa7dadf28e302315dc16e16a91565c43b
JayEffTree <jfinelli3@gmail.com>
2020-06-02 06:03:42 +0200

    reload cool, dir cahnge

---
+++ src/commands/owner/ReloadCommand.js
@@ -0,0 +1,18 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const { reloadCommands } = require('../../utils/registry');
+
+module.exports = class ReloadCommand extends BaseCommand {
+  constructor() {
+    super('reload', 'owner', [], 'Reload commands with this!');
+  }
+
+  async run(client, message, args) {
+
+    let loading = await message.channel.send(client.resource.loading());
+    await reloadCommands(client)
+    loading.edit(client.resource.embed()
+      .setTitle(`Success! <:check:694636220571189389>`)
+      .setDescription(`Reloaded all commands.`)
+    );
+  }
+}
