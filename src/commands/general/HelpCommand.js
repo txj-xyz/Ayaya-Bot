da7beffdec65ec9e2089f5e9b6ab687ee6aa1c78
JayEffTree <jfinelli3@gmail.com>
2020-06-02 05:39:10 +0200

    check for category coming soon

--- src/commands/general/HelpCommand.js
+++ src/commands/general/HelpCommand.js
@@ -8,8 +8,13 @@ module.exports = class HelpCommand extends BaseCommand {
 
   async run(client, message, args) {
     let loading = await message.channel.send(client.resource.loading())
-    await client.commands.forEach(function(value, key) {
-      commandsListing += `\`${process.env.DISCORD_BOT_PREFIX}${key}\` - ${value.description}\n`
+    await client.commands.forEach(function(cmdKey, command) {
+      if(cmdKey.category === 'general'){
+        commandsListing += `\`${process.env.DISCORD_BOT_PREFIX}${command}\` - ${cmdKey.description}\n`
+      }
+      // else {
+      //   console.log(`Key is not 'General' ${command} - ${cmdKey.description}`)
+      // }
     })
     loading.edit(client.resource.embed()
       .setTitle(`Aya Commands\n`)
