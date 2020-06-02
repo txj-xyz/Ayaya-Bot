fb4ad988e82822f56fec30cdc1cc446f00e1715b
JayEffTree <jfinelli3@gmail.com>
2020-06-02 05:17:53 +0200

    starting of yae command

---
+++ src/commands/general/YaeCommand.js
@@ -0,0 +1,28 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const Nekos = require('nekos.life')
+const yae = new Nekos();
+
+module.exports = class PingCommand extends BaseCommand {
+  constructor() {
+    super('yae', 'general', [], 'Get random picture from nekos.life');
+  }
+
+  async run(client, message, args) {
+    //send loading embed
+    let loading = await message.channel.send(client.resource.loading())
+    let img = await yae.sfw.hug();
+
+    try{
+      loading.edit(client.resource.embed()
+        .setImage(img.url)
+      );
+    } catch(e){
+      console.log(`[ERROR] - (YAE) - Failed to retrieve image from API.`)
+      loading.edit(client.resource.embed()
+        .setTitle(`Failed. <:cross:694636239810330724>`)
+        .setDescription('Failed: Error with retreiving photo. Please try again.')
+      )
+    }
+    
+  }
+}
