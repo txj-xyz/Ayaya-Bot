928d7e59c021051d99fb3386f00a4896bae1eaeb
JayEffTree <jfinelli3@gmail.com>
2020-06-02 07:09:49 +0200

    PROBABLY GOING TO REDO THIS DUMB ASS COMMAND

--- src/commands/general/HelpCommand.js
+++ src/commands/general/HelpCommand.js
@@ -11,19 +11,26 @@ module.exports = class HelpCommand extends BaseCommand {
   }
 
   async run(client, message, args) {
+
     let loading = await message.channel.send(client.resource.loading())
-    await client.commands.forEach(function(cmdKey, command) {
-      console.log(cmdKey, command)
-      if(cmdKey.category === 'general'){
-        cmdGeneral += `\`${process.env.DISCORD_BOT_PREFIX}${command}\` - ${cmdKey.description}\n`
-      }
-      else if(cmdKey.category === 'owner'){
-        cmdOwner += `\`${process.env.DISCORD_BOT_PREFIX}${command}\` - ${cmdKey.description}\n`
-      }
-      else if(cmdKey.category === 'neko'){
-        cmdNeko += `\`${process.env.DISCORD_BOT_PREFIX}${command}\` - ${cmdKey.description}\n`
-      }
-    })
+
+    //push to string here with += 
+
+    if(cmdGeneral === '' && cmdOwner === '' && cmdNeko === ''){
+
+      await client.commands.forEach(function(cmdKey, command) {
+        if(cmdKey.category === 'general'){
+          cmdGeneral += `\`${process.env.DISCORD_BOT_PREFIX}${command}\` - ${cmdKey.description}\n`
+        }
+        if(cmdKey.category === 'owner'){
+          cmdOwner += `\`${process.env.DISCORD_BOT_PREFIX}${command}\` - ${cmdKey.description}\n`
+        }
+        if(cmdKey.category === 'neko'){
+          cmdNeko += `\`${process.env.DISCORD_BOT_PREFIX}${command}\` - ${cmdKey.description}\n`
+        }
+      })
+    }
+    
 
     //send help commands
     loading.edit(client.resource.embed()
