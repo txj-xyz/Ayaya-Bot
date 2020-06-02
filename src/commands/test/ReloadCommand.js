e63ca310ffcf979eaccad1bf261f3aeac5b58a3d
JayEffTree <jfinelli3@gmail.com>
2020-06-02 04:37:29 +0200

    fire off reload when requested

--- src/commands/test/ReloadCommand.js
+++ src/commands/test/ReloadCommand.js
@@ -7,7 +7,13 @@ module.exports = class PingCommand extends BaseCommand {
   }
 
   async run(client, message, args) {
-    await reloadCommands(client, '../commands')
-    message.channel.send(`Reloaded all commands`)
+    if(message.author.id !== process.env.DISCORD_BOT_OWNER) return;
+
+    let loading = await message.channel.send(client.resource.loading());
+    await reloadCommands(client)
+    loading.edit(client.resource.embed()
+      .setTitle(`Success! <:check:694636220571189389>`)
+      .setDescription(`Reloaded all commands.`)
+    );
   }
 }
