dd9b3278cda331cb2a9fddc84fa8e96e016b8f44
JayEffTree <jfinelli3@gmail.com>
2020-06-02 05:17:44 +0200

    add in help command

---
+++ src/commands/general/HelpCommand.js
@@ -0,0 +1,19 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+let commandsListing = '';
+
+module.exports = class PingCommand extends BaseCommand {
+  constructor() {
+    super('help', 'general', [], 'Shows this help page!');
+  }
+
+  async run(client, message, args) {
+    let loading = await message.channel.send(client.resource.loading())
+    await client.commands.forEach(function(value, key) {
+      commandsListing += `\`${process.env.DISCORD_BOT_PREFIX}${key}\` - ${value.description}\n`
+    })
+    loading.edit(client.resource.embed()
+      .setTitle(`Aya Commands\n`)
+      .setDescription(commandsListing)
+    );
+  }
+}
