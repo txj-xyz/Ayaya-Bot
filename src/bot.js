ea301d53bf68ef710f4d6cad6c6abd4cd5e858a2
JayEffTree <jfinelli3@gmail.com>
2020-06-02 04:36:34 +0200

    require embed here

--- src/bot.js
+++ src/bot.js
@@ -6,9 +6,10 @@ const client = new Client();
 (async () => {
   client.commands = new Map();
   client.events = new Map();
+  client.resource = require('./utils/embeds');
+  
   client.prefix = process.env.DISCORD_BOT_PREFIX;
   await registerCommands(client, '../commands');
   await registerEvents(client, '../events');
   await client.login(process.env.DISCORD_BOT_TOKEN);
 })();
-
