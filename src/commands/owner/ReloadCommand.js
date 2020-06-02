12405cbea40b99605c659e04fbaa89abe4dc6199
JayEffTree <jfinelli3@gmail.com>
2020-06-02 20:11:06 +0200

    fuck i forgot to remove that

--- src/commands/owner/ReloadCommand.js
+++ src/commands/owner/ReloadCommand.js
@@ -9,11 +9,7 @@ module.exports = class ReloadCommand extends BaseCommand {
   async run(client, message, args) {
 
     let loading = await message.channel.send(client.resource.loading());
-    try{
-      reloadCommands(client)
-    }catch(e){
-      console.log(`WOW DATA: ` + e)
-    }
+    await reloadCommands(client)
     
     loading.edit(client.resource.embed()
       .setTitle(`Success! <:check:694636220571189389>`)
