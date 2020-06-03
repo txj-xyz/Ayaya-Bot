e9db6f882b5377dc9e39f764e57023c5a94d5047
JayEffTree <jfinelli3@gmail.com>
2020-06-03 03:04:34 +0200

    change to loading embed

--- src/commands/music/NowPlayingCommand.js
+++ src/commands/music/NowPlayingCommand.js
@@ -9,7 +9,9 @@ module.exports = class NowPlayingCommand extends BaseCommand {
     super('nowPlaying', 'music', ['np', 'playing'], 'Check what is currently playing.');
   }
 
-  async   run(client, message, args) {
+  async run(client, message, args) {
+    let loading = await message.channel.send(client.resource.loading())
+
     const player = client.music.players.get(message.guild.id);
     if (!player || !player.queue[0]) {
       return message.channel.send(`No song's currently playing.`)
@@ -18,22 +20,21 @@ module.exports = class NowPlayingCommand extends BaseCommand {
     let track = player.queue[0],
       trackInfo = await fetch(track.identifier);
 
-    const currentTrack = new MessageEmbed()
-      .setAuthor(`Now Playing`, `https://i.imgur.com/NKrXtzA.gif`)
-      .setTitle(`${trackInfo.title}`)
-      .setURL(trackInfo.url)
-      .setImage(trackInfo.thumbnailUrl)
+      loading.edit(client.resource.embed()
+        .setAuthor(`Now Playing`, `https://i.imgur.com/NKrXtzA.gif`)
+        .setTitle(`${trackInfo.title}`)
+        .setURL(trackInfo.url)
+        .setImage(trackInfo.thumbnailUrl)
 
-      .addField(`Owner`, trackInfo.owner, true)
-      .addField(`Date Published`, trackInfo.datePublished, true)
-      .addField(`Genre`, trackInfo.genre, true)
-      .addField(`Duration`, Utils.formatTime(track.duration, true), true)
-      .addField(`Views`, trackInfo.views, true)
-      .addField(`Likes | Dislikes`, `${trackInfo.likeCount} **|** ${trackInfo.dislikeCount}`, true)
+        .addField(`Owner`, trackInfo.owner, true)
+        .addField(`Date Published`, trackInfo.datePublished, true)
+        .addField(`Genre`, trackInfo.genre, true)
+        .addField(`Duration`, Utils.formatTime(track.duration, true), true)
+        .addField(`Views`, trackInfo.views, true)
+        .addField(`Likes | Dislikes`, `${trackInfo.likeCount} **|** ${trackInfo.dislikeCount}`, true)
 
-      .setFooter(`Requested by - ${track.requester.username}`, track.requester.avatarURL())
-      .setTimestamp()
-
-    message.channel.send(currentTrack);
+        .setFooter(`Requested by - ${track.requester.username}`, track.requester.avatarURL())
+        .setTimestamp()
+      )
   }
 }
