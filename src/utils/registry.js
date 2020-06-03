43b1c609eb3a95e287b0109aab3a8bddde2bede3
JayEffTree <jfinelli3@gmail.com>
2020-06-03 03:01:32 +0200

    delay the loading of commands a slight bit

--- src/utils/registry.js
+++ src/utils/registry.js
@@ -17,10 +17,12 @@ async function registerCommands(client, dir = '') {
             const cmd = new Command();
             client.commands.set(cmd.name, cmd);
             console.log(`[INFO] Loaded - [${cmd.name} - ${cmd.description}]`)
-            cmd.aliases.forEach(async (alias) => {
-              client.commands.set(alias, cmd);
-              console.log(`[INFO] Loaded alias - [${alias}]`)
-            });
+            setTimeout(()=>{
+              cmd.aliases.forEach(async (alias) => {
+                client.commands.alias.set(alias, cmd);
+                console.log(`[INFO] Loaded alias - [${alias}]`)
+              });
+            }, 500)
           }
         }
       // } catch(e) {
@@ -47,7 +49,7 @@ async function reloadCommands(client, dir = '../commands') {
           console.log(`[INFO] Reloaded - [${cmd.name}]`)
           cmd.aliases.forEach(async (alias) => {
             delete require.cache[require.resolve(path.join(filePath, file))]
-            await client.commands.set(alias, cmd);
+            await client.commands.alias.set(alias, cmd);
             console.log(`[INFO] Reloaded alias - [${alias}]`)
           });
         }
@@ -66,9 +68,11 @@ async function registerEvents(client, dir = '') {
     if (stat.isDirectory()) registerEvents(client, path.join(dir, file));
     if (file.endsWith('.js')) {
       const Event = require(path.join(filePath, file));
+      //console.log(path.join(filePath, file))
       if (Event.prototype instanceof BaseEvent) {
         const event = new Event();
         client.events.set(event.name, event);
+        console.log(event.name)
         client.on(event.name, event.run.bind(event, client));
       }
     }
