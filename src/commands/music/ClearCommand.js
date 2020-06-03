7801765eec340b13c8ad2ccd740405a3e2549a17
JayEffTree <jfinelli3@gmail.com>
2020-06-03 20:58:21 +0200

    Changed clear command to just clear the queue entriely

--- src/commands/music/ClearCommand.js
+++ src/commands/music/ClearCommand.js
@@ -19,37 +19,7 @@ module.exports = class ClearCommand extends BaseCommand {
     if (!voiceChannel || voiceChannel.id !== player.voiceChannel.id) {
       return loading.edit(client.resource.embed().setDescription(`You need to be in the same voice channel as the bot to use this command.`));
     }
-
-    if (args[0]) {
-      let choice = Number(args[0]);
-
-      const checkEmbed = new MessageEmbed()
-        .setAuthor(`Remove Song?`, `https://i.imgur.com/rmha2H7.gif`)
-        .setTitle(`${player.queue[choice].title}`)
-        .setURL(`${player.queue[choice].uri}`)
-
-        .setFooter(`Requested by - ${message.author.username}`,message.author.avatarURL())
-        .setTimestamp()
-
-      const sentMsg = await loading.edit(checkEmbed);
-
-      await sentMsg.react(check);
-      await sentMsg.react(cross);
-
-      const filter = (reaction, user) => [check.name, cross.name].includes(reaction.emoji.name) && (message.author.id === user.id);
-      const collector = sentMsg.createReactionCollector(filter);
-
-      collector.on('collect', (reaction, user) => {
-        if (reaction.emoji.name === check.name) {
-          player.queue.clear();
-        }
-        else if (reaction.emoji.name === cross.name) {
-          collector.stop();
-        }
-      });
-    }
-    else {
-      return loading.edit(client.resource.embed().setDescription(`Please provide a song number to remove.`));
-    }
+    await player.queue.clear();
+    loading.edit(client.resource.embed().setDescription(``))
   }
 }
