1887006a0bd6b22129120017c13917b5d02070b2
JayEffTree <jfinelli3@gmail.com>
2020-06-03 02:57:25 +0200

    alias command map

--- src/bot.js
+++ src/bot.js
@@ -5,11 +5,12 @@ const client = new Client();
 
 (async () => {
   client.commands = new Map();
+  client.commands.alias = new Map(); //setup new map for aliases specifically
   client.events = new Map();
   client.resource = require('./utils/embeds');
-  
   client.prefix = process.env.DISCORD_BOT_PREFIX;
   await registerCommands(client, '../commands');
   await registerEvents(client, '../events');
   await client.login(process.env.DISCORD_BOT_TOKEN);
+  //client.music.on("nodeConnect", node => console.log("[READY] Connected to LavaLink Server"));
 })();
