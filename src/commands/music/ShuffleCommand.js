2b6cc98dbef3821fac7753809286e1af6a871f96
JayEffTree <jfinelli3@gmail.com>
2020-06-03 18:27:15 +0200

    added loading embed

--- src/commands/music/ShuffleCommand.js
+++ src/commands/music/ShuffleCommand.js
@@ -7,14 +7,16 @@ module.exports = class ShuffleCommand extends BaseCommand {
   }
 
   run(client, message, args) {
+    let loading = await message.channel.send(client.resource.loading())
+
     const player = client.music.players.get(message.guild.id);
     if (!player || !player.queue[0]) {
-      return message.channel.send(`No song's currently playing.`);
+      return loading.edit(client.resource.embed().setDescription(`No song's currently playing.`));
     }
 
     const voiceChannel  = message.member.voice.channel;
     if (!voiceChannel || voiceChannel.id !== player.voiceChannel.id) {
-      return message.channel.send(`You need to be in the same voice channel as the bot to use this command.`);
+      return loading.edit(client.resource.embed().setDescription(`You need to be in the same voice channel as the bot to use this command.`));
     }
 
     player.queue.shuffle();
@@ -33,7 +35,7 @@ module.exports = class ShuffleCommand extends BaseCommand {
         .setFooter(`Requested by - ${message.author.username}`,message.author.avatarURL())
         .setTimestamp()
 
-      message.channel.send(newQueueEmbed)
+      loading.edit(newQueueEmbed)
     }
     else {
       let list = ``,
@@ -49,7 +51,7 @@ module.exports = class ShuffleCommand extends BaseCommand {
         .setFooter(`Requested by - ${message.author.username}`,message.author.avatarURL())
         .setTimestamp()
 
-      message.channel.send(newQueueEmbed)
+      loading.edit(newQueueEmbed)
     }
   }
 }
