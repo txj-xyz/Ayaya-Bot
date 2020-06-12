53e8686c3a73df1b72d6432624ad5ce3de683b35
JayEffTree <jfinelli3@gmail.com>
2020-06-12 17:57:06 +0200

    see if this works but more logging

--- src/events/music.js
+++ src/events/music.js
@@ -13,18 +13,15 @@ module.exports = class MusicEvent extends BaseEvent {
   async run (client) {
     client.livePlaying = new Map();
     client.music = new ErelaClient(client, nodes)
-    .on("nodeConnect", node => console.log("[READY] Connected to LavaLink Server"))
+    .on("nodeConnect", node => console.log("[MUSIC][READY] Connected to LavaLink Server"))
     .on("queueEnd", player => {
       player.textChannel.send(client.resource.embed()
         .setDescription(`The queue hath finshed sire. <@189238841054461952>`)
       )
       client.music.players.destroy(player.guild.id);
+    })
+    .on("trackStart", (player, track) => {
+      console.log(`[MUSIC][INFO] - Song has been started.`)
     });
-    // .on("trackStart", (player, track) => {
-    //   client.livePlaying.set(`track`, track)
-    //   player.textChannel.send(client.resource.embed()
-    //     .setDescription(`Now playing: ${track.title}`)
-    //   )
-    // });
   }
 }
