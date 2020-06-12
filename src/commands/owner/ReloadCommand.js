9034fbad895ce0890c9c49b97e09b0b7dcc021f2
JayEffTree <jfinelli3@gmail.com>
2020-06-12 17:57:20 +0200

    add reload fail support

--- src/commands/owner/ReloadCommand.js
+++ src/commands/owner/ReloadCommand.js
@@ -6,15 +6,15 @@ module.exports = class ReloadCommand extends BaseCommand {
     super('reload', 'owner', [], 'Reload commands with this!');
   }
 
-  async run(client, message, args) {
-
+  async run(client, message) {
+    client.loadfail = [];
     let loading = await message.channel.send(client.resource.loading());
-    await reloadCommands(client)
-    
+    reloadCommands(client);
     loading.edit(client.resource.embed()
       .setTitle(`<:check:694636220571189389> Success!`)
       .addField(`Alias Commands`, `\`${client.alias.size}\` reloaded.`, false)
       .addField(`Base Commands`, `\`${client.commands.size}\` reloaded.`, false)
-    );
-  }
+      .addField(`Failed Commands`, `\`${client.loadfail.length}\` failed to load due to an error`, false)
+      .setDescription(`\`\`\`js\n${client.loadfail.join("\n")}\n\`\`\``));
+    }
 }
