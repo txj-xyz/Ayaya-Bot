6fc9e7f4e1acab837f7fa8aac4203714c87b2856
JayEffTree <jfinelli3@gmail.com>
2020-06-23 01:17:05 +0200

    more logging, wow

--- src/events/ready.js
+++ src/events/ready.js
@@ -8,5 +8,10 @@ module.exports = class ReadyEvent extends BaseEvent {
     client.user.setPresence({ activity: { type: 'LISTENING', name: `${client.guilds.cache.size} servers. | ${process.env.DISCORD_BOT_PREFIX}help` }})
     console.log(`[INFO] - Presence set: LISTENING to ${client.guilds.cache.size} servers. | ${process.env.DISCORD_BOT_PREFIX}help`)
     console.log(client.user.tag + ' has logged in.');
+    client.resource.sendLive(client, process.env.LIVE_CHANNEL,
+      `[INFO]`,
+      `Presence set: LISTENING to ${client.guilds.cache.size} servers. | ${process.env.DISCORD_BOT_PREFIX}help`+
+      `${client.user.tag} has logged in.`
+      )
   }
 }
