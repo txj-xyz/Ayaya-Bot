7ffbd618937704ecacacddad209ceee1582350b0
JayEffTree <jfinelli3@gmail.com>
2020-06-23 01:17:30 +0200

    join/leave embeds

--- src/utils/embeds.js
+++ src/utils/embeds.js
@@ -16,5 +16,38 @@ module.exports = (client) = {
         .setTitle(`Loading data<a:embed_loading:695358635110301726>`)
         .setURL(`https://discord.gg/CSJkCGx`);
         return embed
+    },
+    leave(guild){
+        const embed = new Discord.MessageEmbed()
+        .setColor('#ff0000')
+        .setTitle(`Left Guild!`)
+        .addField(`Guild Name`, guild.name, false)
+        .addField(`Guild ID`, guild.id, false)
+        .addField(`Member Count`, guild.memberCount - 1, true)
+        .addField("Humans", `${guild.members.cache.filter(member => !member.user.bot).size} `, true)
+        .addField("Bots", `${guild.members.cache.filter(member => member.user.bot).size}` - 1, true)
+        .addField(`Owner`, `${guild.owner.user.tag}[${guild.owner.id}]`, false)
+        return embed
+    },
+    join(guild){
+        const embed = new Discord.MessageEmbed()
+        .setColor('#26ff00')
+        .setTitle(`Join Guild!`)
+        .addField(`Guild Name`, guild.name, false)
+        .addField(`Guild ID`, guild.id, false)
+        .addField(`Member Count`, guild.memberCount - 1, true)
+        .addField("Humans", `${guild.members.cache.filter(member => !member.user.bot).size} `, true)
+        .addField("Bots", `${guild.members.cache.filter(member => member.user.bot).size}` - 1, true)
+        .addField(`Owner`, `${guild.owner.user.tag}[${guild.owner.id}]`, false)
+        return embed
+    },
+    sendLive(client, channelid, name, body){
+        client.channels.cache.get(channelid).send(
+            new Discord.MessageEmbed()
+            .setColor('#d2eb34')
+            .setTitle(`${name}`)
+            .setURL(`https://discord.gg/CSJkCGx`)
+            .setDescription(`\`\`\`js\n${body}\n\`\`\``)
+        );
     }
 }
