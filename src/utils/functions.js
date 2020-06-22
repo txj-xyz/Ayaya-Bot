c2493a965354695b1b798d2685285a1eec9532a7
JayEffTree <jfinelli3@gmail.com>
2020-06-23 01:17:55 +0200

    honestly, I'm removing this next update

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,13 +1,13 @@
-module.exports = {
+module.exports = (client) = {
     async checkUser(guild, user) {
-        const member = guild.members.cache.get(user.id);
-        let mixerRole = await guild.roles.cache.find(role => role.name === "🎵 Mixer 🎵");
+      const member = guild.members.cache.get(user.id);
+      let mixerRole = await guild.roles.cache.find(role => role.name === "🎵 Mixer 🎵");
 
-        if (member.hasPermission(`ADMINISTRATOR`) || member.roles.cache.has(mixerRole.id)) {
-            return true;
-        }
-        else {
-            return false;
-        }
+      if (member.hasPermission(`ADMINISTRATOR`) || member.roles.cache.has(mixerRole.id)) {
+        return true;
+      }
+      else {
+        return false;
+      }
     }
 };
